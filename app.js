b0551e9be12b62b1107ff96354d535008aae780f
Shyam-design634 <meghashyamdora.hanumanthu@slu.edu>
2024-11-12 22:25:46 +0000

    Committed code change to get the output as needed.

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const port = process.env.PORT || 3000;
 
 // Start server with request and response handling
 const server = http.createServer(function (request, response) {
-  response.end("hi");
+    response.setHeader('Content-Type', 'application/json');
+  
+    // Define a JavaScript object to send as JSON
+    const data = {
+      text: 'hi',
+      number: [1, 2, 3]
+    };
+    // Convert the object to a JSON string and send it as the response
+  response.end(JSON.stringify(data));
 });
 
 // Listen on the specified port
